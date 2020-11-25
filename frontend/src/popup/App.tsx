f6f5ebb88a194a368e93e14165772630e2f55507
Ilya <iliakrapivkin@gmail.com>
2020-11-25 23:57:02 +0300

    new nmps_popup works

---
+++ frontend/src/popup/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+// import React, { useEffect } from 'react';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { State } from './app/store/states/types';
+// import { authorise } from './app/store/states/domainData/slices/auth/auth';
+// import Capybara from './app/Components/Capybara/Capybara';
+// import Overlay from './app/Components/Overlay/Overlay';
+// import { Overlay as OverlayState } from './app/store/states/ui/slices/overlay/types';
+// import './App.scss';
+
+const App: React.FC = () => {
+  return <div className="App">priviet popup!</div>;
+};
+
+export default App;
